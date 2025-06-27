36387862792ef3970af5c919b54e8df1c2a8a2fe
Prithveer1-Sirpi <prithveer@sirpi.io>
2025-06-27 11:37:51 +0530

    Footer and header link changes

--- include/client/header.inc.php
+++ include/client/header.inc.php
@@ -161,7 +161,7 @@ if (($lang = Internationalization::getCurrentLanguage())) {
 
                             $initials = strtoupper(substr($thisclient->getName(), 0, 1) . (strpos($thisclient->getName(), ' ') !== false ? substr($thisclient->getName(), strpos($thisclient->getName(), ' ') + 1, 1) : ''));
 
-                            echo '<a href="' . ROOT_PATH . 'profile.php" style="
+                            echo '<a style="
     display: inline-flex;
     justify-content: center;
     align-items: center;
@@ -175,6 +175,7 @@ if (($lang = Internationalization::getCurrentLanguage())) {
     margin: 0 5px;
     text-transform: uppercase;
     text-decoration: none;
+    cursor: default;
                         ">' . Format::htmlchars($initials) . '</a>';
                         } elseif ($nav) {
                             if ($cfg->getClientRegistrationMode() != 'disabled') {
