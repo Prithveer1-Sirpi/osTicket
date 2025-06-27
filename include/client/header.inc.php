fb44a7a35f5cda651e20ef6d6bd357cb4b96a685
Prithveer1-Sirpi <prithveer@sirpi.io>
2025-06-27 11:04:31 +0530

    Support center home - header restructured and logo link corrected

--- include/client/header.inc.php
+++ include/client/header.inc.php
@@ -235,7 +235,7 @@ if (($lang = Internationalization::getCurrentLanguage())) {
                         } ?>
                     </p>
                 </div>
-                <a class="pull-left" id="logo" href="<?php echo ROOT_PATH; ?>index.php"
+                <a class="pull-left" id="logo" href="https://tgdex.telangana.gov.in/"
                     title="<?php echo __('Support Center'); ?>">
                     <span class="valign-helper"></span>
                     <img src="<?php echo ROOT_PATH; ?>logo.php" border=0 alt="<?php
